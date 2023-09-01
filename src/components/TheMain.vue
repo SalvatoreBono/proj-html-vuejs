965c209e84a6d718dfc280a48965a6c93742b54f
SalvatoreBono <salvatore.bono2001@libero.it>
2023-09-01 02:39:24 +0200

    add section

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -9,6 +9,72 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="container text-center pt-5">
+    <h6 class="fw-bold">PELICULA STUDIO</h6>
+    <h3 class="fw-bold pb-5 pt-3">THIS IS WHAT WE DO</h3>
+    <div class="row justify-content-between">
+      <div class="structure">
+        <img src="../assets/image (14).svg" alt="" />
+        <h4 class="fw-bold pt-4 pb-2">ARRAY OF EQUIPMENT</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eos,
+          provident recusandae velit
+        </p>
+      </div>
+      <div class="structure">
+        <img src="../assets/image (16).svg" alt="" />
+        <h4 class="fw-bold pt-4 pb-2">NEW TECHNOLOGIES</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eos,
+          provident recusandae velit
+        </p>
+      </div>
+      <div class="structure">
+        <img src="../assets/image (18).svg" alt="" />
+        <h4 class="fw-bold pt-4 pb-2">VERSITILE ACTORS</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eos,
+          provident recusandae velit
+        </p>
+      </div>
+    </div>
+    <div class="row justify-content-between">
+      <div class="structure">
+        <img src="../assets/image (15).svg" alt="" />
+        <h4 class="fw-bold pt-4 pb-2">TOP DIRECTORS</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eos,
+          provident recusandae velit
+        </p>
+      </div>
+      <div class="structure">
+        <img src="../assets/image (17).svg" alt="" />
+        <h4 class="fw-bold pt-4 pb-2">MINUTE EDITING</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eos,
+          provident recusandae velit
+        </p>
+      </div>
+      <div class="structure">
+        <img src="../assets/image (19).svg" alt="" />
+        <h4 class="fw-bold pt-4 pb-2">VERSITILE ACTORS</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eos,
+          provident recusandae velit
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.structure {
+  width: calc(100% / 3 - 2rem);
+  padding-bottom: 3rem;
+}
+img {
+  width: 80px;
+  height: 80px;
+}
+</style>
